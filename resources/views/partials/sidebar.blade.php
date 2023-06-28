88d84e64000b26a09ff3673bf2f83ccb0813b326
firdauszulkarnain <muhammadfirdauszulkarnain03@gmail.com>
2023-06-28 21:05:45 +0800

    initial commit

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,21 @@
+
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-text mx-3">ADMINISTRATOR</div>
+    </a>
+
+    <hr class="sidebar-divider my-0">
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <hr class="sidebar-divider">
+
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
